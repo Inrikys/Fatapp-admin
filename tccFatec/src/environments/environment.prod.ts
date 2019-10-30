19919452679b89390ab1908d71c66bb27e01800f
Henrique Sousa <inrikys@gmail.com>
2019-10-30 10:22:27 -0300

    Lista de presença att e enviar e-mail

--- tccFatec/src/environments/environment.prod.ts
+++ tccFatec/src/environments/environment.prod.ts
@@ -1,10 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDY3XySQofefS6rVyuz-7rocX-0hQgaYok\n",
-    authDomain: "tccfatecscs2019-feeaf.firebaseapp.com",
-    databaseURL: "https://tccfatecscs2019-feeaf.firebaseio.com",
-    projectId: "tccfatecscs2019-feeaf",
-    storageBucket: "tccfatecscs2019-feeaf.appspot.com",
-  }
+    apiKey: 'AIzaSyDY3XySQofefS6rVyuz-7rocX-0hQgaYok\n',
+    authDomain: 'tccfatecscs2019-feeaf.firebaseapp.com',
+    databaseURL: 'https://tccfatecscs2019-feeaf.firebaseio.com',
+    projectId: 'tccfatecscs2019-feeaf',
+    storageBucket: 'tccfatecscs2019-feeaf.appspot.com',
+  },
+  apiCoreUrl: 'ec2-54-188-117-176.us-west-2.compute.amazonaws.com/',
+  // tslint:disable-next-line:max-line-length
+  apiCoreToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsImVtYWlsIjoiaGVucmlxdWU2NjFAZ21haWwuY29tIiwiaWF0IjoxNTcyMzk4NjM3fQ.D1PIOdOm1Uh7iTgKmM-pTQPzU5qc8hsCtHWwVCtjIjw',
 };
